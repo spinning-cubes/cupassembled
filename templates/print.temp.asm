5708d2849af620fe2272e4709092d413e8eec579
Spinning Cubes <spiin100@outlook.com>
2025-07-26 21:58:17 -0400

    Create print.temp.asm

---
+++ templates/print.temp.asm
@@ -0,0 +1,21 @@
+org 0x7c00
+
+section .data
+    msg db "[[TEXT]]", 0
+
+section .text
+    global _start
+
+_start:
+    mov si, msg
+
+print_loop_[[NAME]]:
+    mov al, [si]
+    cmp al, 0
+    je end_print
+
+    mov ah, 0x0E
+    int 0x10
+
+    inc si
+    jmp print_loop_[[NAME]]

a512c2e3ffc40418afe129492512513246e8b2f0
Spinning Cubes <spiin100@outlook.com>
2025-07-26 22:05:34 -0400

    Update print.temp.asm

--- templates/print.temp.asm
+++ templates/print.temp.asm
@@ -10,10 +10,13 @@ _start:
 print_loop_[[NAME]]:
     mov al, [si]
     cmp al, 0
-    je end_print
+    je end_print_[[NAME]]
 
     mov ah, 0x0E
     int 0x10
 
     inc si
     jmp print_loop_[[NAME]]
+
+end_print_[[NAME]]:
+    ret
